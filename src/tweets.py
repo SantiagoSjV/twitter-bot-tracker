3761cf9012e193073ea12221fc864fac747fbed1
SantiagoSjV <ginsanjuanva@gmail.com>
2023-03-05 00:00:18 -0600

    upload multiple photos in the same tweet

--- src/tweets.py
+++ src/tweets.py
@@ -3,15 +3,19 @@ import requests
 import snscrape.modules.twitter as sntwitter
 
 def saveMedia(tweetMedia):
+    media_url = []
+    index = 0
     for medium in tweetMedia:
         if isinstance(medium, sntwitter.Photo):
+            index += 1
             mediaFolder = f'{tweet.user.username}'
-            mediaName = f'{tweet.id}.png'
+            mediaName = f'{tweet.id}_{index}.png'
             r = requests.get(medium.fullUrl)
             with open(f"media/{mediaName}", 'wb') as fp:
                 fp.write(r.content)
             fb.connection.storage().child(f'{mediaFolder}/{mediaName}').put(f'media/{mediaName}')
-            return fb.connection.storage().child(f'{mediaFolder}/{mediaName}').get_url(None)
+            media_url.append(fb.connection.storage().child(f'{mediaFolder}/{mediaName}').get_url(None))
+    return media_url
 
 query = "#food"
 currentId = 0
@@ -20,6 +24,6 @@ while 1:
     for tweet in sntwitter.TwitterSearchScraper(query).get_items():
         currentId = tweet.id
         if currentId != lastId and tweet.media:
-            print("\nmediaUrl: " + saveMedia(tweet.media))
-            print("content: " + tweet.rawContent)
+            print(saveMedia(tweet.media))
+            print("content: " + tweet.rawContent + "\n")
         break
